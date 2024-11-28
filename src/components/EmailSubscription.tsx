d504de7cfa7f4049d07875720adeb8c39d2156b1
pkkwilliam <24216676+pkkwilliam@users.noreply.github.com>
2024-11-27 18:03:14 -0800

    allow subscription to refresh

--- src/components/EmailSubscription.tsx
+++ src/components/EmailSubscription.tsx
@@ -71,7 +71,12 @@ const EmailSubscription = () => {
                   autoComplete="email"
                   className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm/6"
                   value={email}
-                  onChange={(e) => setEmail(e.currentTarget.value)}
+                  onChange={(e) => {
+                    if (subscribed) {
+                      setSubscribed(false);
+                    }
+                    setEmail(e.currentTarget.value);
+                  }}
                 />
                 <button
                   disabled={loading || subscribed}
